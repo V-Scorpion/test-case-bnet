970d1c595ac086b545c70282d41e58d9b1ba75f8
V-Scorpion <57521073+V-Scorpion@users.noreply.github.com>
2021-10-15 13:56:33 +0300

    Initial commit

---
+++ app/src/main/java/com/v_scorpion/test_case_bnet/Fragments_add_todo.java
@@ -0,0 +1,22 @@
+package com.v_scorpion.test_case_bnet;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by v_scorpion on 15.10.2021.
+ */
+public class Fragments_add_todo  extends Fragment {
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
